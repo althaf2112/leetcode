49f96adf8e35568897ed24a3a47346755d3663df
Althafmhd <althafmhddq@gmail.com>
2025-11-07 11:02:41 +0530

    Time: 59 ms (6.06%), Space: 55.8 MB (25.29%) - LeetHub

--- 2805-array-wrapper/2805-array-wrapper.js
+++ 2805-array-wrapper/2805-array-wrapper.js
@@ -55,4 +55,41 @@ class ArrayWrapper {
   toString() {
     return `[${this.nums.join(',')}]`;
   }
+}class Calculator {
+  constructor(value) {
+    this.result = value;
+  }
+
+  add(value) {
+    this.result += value;
+    return this; // allows method chaining
+  }
+
+  subtract(value) {
+    this.result -= value;
+    return this;
+  }
+
+  multiply(value) {
+    this.result *= value;
+    return this;
+  }
+
+  divide(value) {
+    if (value === 0) {
+      throw new Error("Division by zero is not allowed");
+    }
+    this.result /= value;
+    return this;
+  }
+
+  power(value) {
+    this.result = Math.pow(this.result, value);
+    return this;
+  }
+
+  getResult() {
+    return this.result;
+  }
 }
+
