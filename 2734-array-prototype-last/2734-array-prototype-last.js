28e09345b4edfa5e0b10a14fa4b9f714b57f25fd
Althafmhd <althafmhddq@gmail.com>
2025-11-10 11:09:07 +0530

    Time: 48 ms (37.11%), Space: 54.3 MB (17.13%) - LeetHub

---
+++ 2734-array-prototype-last/2734-array-prototype-last.js
@@ -0,0 +1,19 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    
+};Array.prototype.last = function() {
+  // 'this' refers to the current array
+  if (this.length === 0) {
+    return -1;
+  }
+  return this[this.length - 1];
+};
+
+
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
